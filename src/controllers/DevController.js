176ec95ddfe4d9daab6f5c316e93adb043660524
Ricardo <ricardofrr@gmail.com>
2020-01-21 08:00:45 -0300

    Adicionado função de remover dev

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,70 +3,75 @@ const Dev = require('../models/Dev')
 const parseStringAsArray = require('../utils/parseStringAsArray')
 const { findConnection, sendMessage } = require('../websocket')
 
-module.exports = {
-    async index(request, response){
-        const devs = await Dev.find()
+async function index(request, response){
+    const devs = await Dev.find()
 
-        return response.json(devs)
-    },
-    async store(request, response) {
-        const { github_username, techs, latitude, longitude } = request.body
-
-        let dev = await Dev.findOne({github_username})
-
-        if(!dev){
-            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-            const { name = login, avatar_url, bio } = apiResponse.data
-            const techsArray = parseStringAsArray(techs)
-        
-            const location = {
-                type: 'Point',
-                coordinates: [latitude, longitude]
-            }
-            dev = await Dev.create({
-                github_username,
-                name,
-                avatar_url,
-                bio,
-                techs: techsArray,
-                location
-            })
-
-            const sendSocketMessageTo = findConnection({latitude, longitude}, techsArray)
-            sendMessage( sendSocketMessageTo, 'new-dev', dev )
-
-        }
-    
-        console.log( dev )
-        return response.json( dev )
-    },
-    async update(request, response){
-        const { techs, latitude, longitude } = request.body
-        const { id } = request.params
-
-        const dev = await Dev.findOne({_id: id})
-        dev.techs = parseStringAsArray( techs )
-        dev.location = {
-            type: "Point",
-            coordinates: [ latitude, longitude ]
-        }
-
-        await dev.save()
-
-        return response.json( dev )
-    },
-    async show(request, response){
-        const { id } = request.params
-
-        const dev = await Dev.findOne({_id: id})
-
-        return response.json( dev )
-    },
-    async destroy(request, response){
-        const { id } = request.params
-
-        await Dev.deleteOne({ _id: id })
-
-        return response.json( { message: `Deleted ${id}` } )
-    }
+    return response.json(devs)
 }
+
+async function store(request, response) {
+    const { github_username, techs, latitude, longitude } = request.body
+
+    let dev = await Dev.findOne({github_username})
+
+    if(!dev){
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+        const { name = login, avatar_url, bio } = apiResponse.data
+        const techsArray = parseStringAsArray(techs)
+    
+        const location = {
+            type: 'Point',
+            coordinates: [latitude, longitude]
+        }
+        dev = await Dev.create({
+            github_username,
+            name,
+            avatar_url,
+            bio,
+            techs: techsArray,
+            location
+        })
+
+        const sendSocketMessageTo = findConnection({latitude, longitude}, techsArray)
+        sendMessage( sendSocketMessageTo, 'new-dev', dev )
+
+    }
+
+    console.log( dev )
+    return response.json( dev )
+}
+
+async function update(request, response){
+    const { techs, latitude, longitude } = request.body
+    const { id } = request.params
+
+    const dev = await Dev.findOne({_id: id})
+    dev.techs = parseStringAsArray( techs )
+    dev.location = {
+        type: "Point",
+        coordinates: [ latitude, longitude ]
+    }
+
+    await dev.save()
+
+    return response.json( dev )
+}
+
+async function show(request, response){
+    const { id } = request.params
+
+    const dev = await Dev.findOne({_id: id})
+
+    return response.json( dev )
+}
+
+async function destroy(request, response){
+    const { id } = request.params
+
+    await Dev.deleteOne({ _id: id })
+
+    index(request, response)
+
+}
+
+module.exports = { index, store, update, show, destroy }
